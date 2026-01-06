885669bb372a0919ed2ebf32fe1e37163770b7b5
Miguel Camilo <miguelcamilo48@gmail.com>
2026-01-05 23:40:15 -0500

    feat: adding vercel analytics for web tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+
 import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -71,6 +73,7 @@ export default function RootLayout({
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
